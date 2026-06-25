3f063e62fc2b491071c36f84f9f54f0ea9dca2c1
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-06-25 13:43:11 -0300

    feat: Criar Strings
    fonte: https://www.php.net/manual/pt_BR/language.types.string.php

---
+++ Sessão 1/string.php
@@ -0,0 +1,26 @@
+<?php
+
+//STRINGS:
+
+echo 'isto é uma string comum';
+
+echo 'Você pode incluir novas linhas em strings
+dessa maneira que estará
+tudo bem.';
+
+//Exibe: Arnold disse uma vez: "I'll be back"
+echo 'Arnold disse uma vez: "I\'ll be back"';
+
+//Exibe: Você excluiu C:\*.*?
+echo 'Você excluiu C:\\*.*?';
+
+//Exibe: Você excluiu C:\*.*?
+echo 'Você excluiu C:\*.*?';
+
+//Exibe: Isto não será substituido por \n uma nova linha
+echo 'Isto não será substituído por \n uma nova linha';
+
+//Exibe: Variáveis $também não $expandem
+echo 'Variáveis $também não $expandem';
+
+?>
