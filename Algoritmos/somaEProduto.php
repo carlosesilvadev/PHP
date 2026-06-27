6ff104e157a60f29d20f7b7bf7cf684eb9ec4054
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-06-27 14:04:09 -0300

    feat: Somar e Multiplicar com constante

---
+++ Algoritmos/somaEProduto.php
@@ -0,0 +1,10 @@
+<?php
+/*Programa que calcule e exiva na tela a soma e o produto de dois números reais, a saber: 91,37 e 7,351*/
+
+$num1 = 91.37;
+$num2 = 7.351;
+
+echo "Soma: $num1 + $num2: ".$num1 + $num2."<br>";
+echo "Produto: $num1 * $num2: ".$num1 * $num2;
+
+?>
