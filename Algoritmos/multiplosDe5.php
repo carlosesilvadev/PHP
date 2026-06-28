25affddf53cfef3aa39d5480e9aeb65c80587abc
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-06-28 12:53:53 -0300

    feat: Calcular Múltiplos de 5 até 50

---
+++ Algoritmos/multiplosDe5.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Programa que calcule e imprima os 50 primeiros múltiplos de 5 maiores que zero.
+ * 
+ */
+
+for($contador=1;$contador<=50;$contador++){
+    echo "$contador X 5 = ".$contador*5,"\n";
+}
