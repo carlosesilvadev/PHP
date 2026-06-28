f77ac21cb716d0b0c3c0bf9c068f1bd5f3bb01f0
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-06-27 21:54:49 -0300

    feat: Exibir Dia da Semana Por Extenso de 1-7

---
+++ Algoritmos/diaDaSemana.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Pedir a digitação de um número que represente o dia da semana e exibindo-o por extenso.
+ */
+
+$diaDaSemana = readline("Digite um número de 1-7: ");
+
+switch($diaDaSemana){
+    case 1:
+        echo "Domingo"; break;
+    case 2:
+        echo "Segunda-feira"; break;
+    case 3:
+        echo "Terça-feira"; break;
+    case 4:
+        echo "Quarta-feira"; break;
+    case 5:
+        echo "Quinta-feira"; break;
+    case 6:
+        echo "Sexta-feira"; break;
+    case 7:
+        echo "Sábado"; break;
+    default:
+        echo "Digite um valor válido!";
+}
