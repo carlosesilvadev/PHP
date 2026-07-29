e431f0b55eadfca7844d881f0aac03362c673cf1
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-07-29 11:52:34 -0300

    feat: Desafio 2 - Criar e Ler Arquivo JSON
    Aprendi a utilizar a flag JSON_PRETTY_PRINT para formatar o json para facilitar a leitura dos dados

--- projetos/Lista de Tarefas/criarLerArquivo.php
+++ projetos/Lista de Tarefas/criarLerArquivo.php
@@ -1,7 +1,33 @@
 <?php
 
-$arquivo = "meuArquivo.txt";
+$array = [
+    'nome' => 'Carlos',
+    'Idade' => '32',
+    'Sexo' => 'M'
+    ];
 
-$conteudoDoArquivo = file_get_contents($arquivo);
+$meuJson = json_encode($array);
 
-echo $conteudoDoArquivo;
+echo "$meuJson\n";
+
+$herois = [
+        "squadName" => "Grupo de Super Heróis",
+        "homeTown" => "São Paulo",
+        "membros" => [
+            "nome" => "Homem Molecula",
+            "idade" => 29
+        ],
+];
+
+//JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK -> Utilizar para corrigir caracteres especiais.
+$heroisJson = json_encode($herois, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
+
+echo "$heroisJson\n";
+
+//Gerando o arquivo JSON
+file_put_contents("herois.json", $heroisJson, FILE_APPEND);
+
+//Lendo o arquivo JSON que foi gerado
+$heroisDoArquivo = file_get_contents("herois.json");
+
+echo $heroisDoArquivo;
