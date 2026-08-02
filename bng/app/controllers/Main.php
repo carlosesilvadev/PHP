efff83ec22b56d8804b9e804af756f1df645d637
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-08-02 15:47:35 -0300

    feat: Corrigir a exigência do id no metodo index, atribuindo sempre null
    Mas se o usuário indicar um id então null é substituido pelo valor indicado pelo usuário na chamada do método

--- bng/app/controllers/Main.php
+++ bng/app/controllers/Main.php
@@ -3,10 +3,10 @@
 namespace bng\Controllers;
 
 class Main{
-    public function index(){
+    public function index($id = null){
         echo "Estou dentro do controlador Main - index";
         echo "<br>";
-        echo "O id indicado foi";
+        echo "O id indicado foi $id";
 
         //Pode chamar qualquer função da pasta app/helpers/Functions.php, depois de incluir o arquivo em composer.json
     }
