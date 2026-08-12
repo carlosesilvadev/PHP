89f0c4dbe8389ef26c69bd82e175e8fb46d1ce8b
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-08-12 18:18:53 -0300

    feat: Iniciar Projeto do Blog
    Aprendi a comentar código com #

---
+++ blog/index.php
@@ -0,0 +1,12 @@
+<?php
+#Arquivo de inicialização do Sistema
+
+echo '<h1>Arquivo index</h1>';
+//Comentário de 1 linha
+echo "Texto dentro de aspas duplas";
+
+#Comentário de 1 linha
+print '<p>texto usando print</p>';
+
+/** Comentário de multiplas linhas */
+print "<small>Texto dentro de aspas duplas no print</small>";
