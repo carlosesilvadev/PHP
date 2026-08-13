02883a313e1bcab667bdaaefb96e4ffa60b82164
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-08-12 22:17:09 -0300

    feat: Criar Tipos de Dados e Determinar tipo de retorno da função
    Aprendi a incluir no final da função o tipo de dado que deve ser retornado com ():string, ():bool, ():int, ():float

--- blog/Helpers.php
+++ blog/Helpers.php
@@ -4,7 +4,8 @@ function saudacao(){
     return 'boa noite';
 }
 
-function resumirTexto($texto, $limite, $continue = '...'){
+#():string; ():int; ():bool -> Especifica qual é o tipo de dado que a função deve retornar.
+function resumirTexto(string $texto, int $limite, string $continue = '...'):string{
 
     return $texto;
 }
--- blog/index.php
+++ blog/index.php
@@ -1,12 +1,23 @@
 <?php
 #Arquivo de inicialização do Sistema
 
+/*Determina usar tipos de dados especificados,
+evitando situações de conversão padrão do PHP
+por exemplo de número para string*/
+//declare(strict_types = 1);
 include 'sistema/configuracao.php';
 require "Helpers.php";
 
-$texto = "texto para resumir dentro de uma variável";
-$texto = 'xxx';
+$texto = 'Texto para resumir';
 
+$string = 'texto';
+$int = 10;
+$float = 9.99;
+$bool = false;
+$nulo = null;
+
+var_dump($string);
+echo '<hr>';
 #Funções estão dentro do arquivo Helpers.php
 echo saudacao();
 echo '<hr>';
