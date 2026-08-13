9fd71d6b15e7a40f0d5dd6e4e2046b1a3071e1e7
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-08-12 22:23:45 -0300

    feat: Criar estrutura de condição IF

--- blog/Helpers.php
+++ blog/Helpers.php
@@ -1,7 +1,18 @@
 <?php
 
 function saudacao(){
-    return 'boa noite';
+    $hora = 2;
+    $saudacao = '';
+
+    if($hora >= 0 && $hora <= 5){
+        $saudacao = 'boa madrugada';
+    }
+    
+    if($hora >= 6 && $hora <= 12){
+        $saudacao = 'bom dia';
+    }
+
+    return $saudacao;
 }
 
 #():string; ():int; ():bool -> Especifica qual é o tipo de dado que a função deve retornar.
