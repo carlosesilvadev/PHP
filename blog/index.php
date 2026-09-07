a926311fabfc12a146f6bc908107049f17d3dcee
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-09-06 21:40:01 -0300

    :sparkles: feat(index/for): Desenvolver laço de repetição for para exibir a tabuada
    Oportunidade: Exemplificar o laço de repetição
    
    Aprendizado: Conhecer o algoritmo de validação do CPF e tive o insight que a maneira mais segura para validar um cpf seria integração com a API SERPRO do governo para ser mais assertivo quanto a validação.

--- blog/index.php
+++ blog/index.php
@@ -6,4 +6,21 @@
 include 'sistema/configuracao.php';
 require "Helpers.php";
 
-echo saudacao();
+$numero = 1;
+
+/* while($numero > 0){
+    echo $numero++;
+} */
+
+for($contador = 1; $contador <= 10; $contador++){
+    echo (!($contador%2) ? $contador.' é par' : $contador.' é impar')."<br>";
+}
+
+echo "<br>";
+
+for($tabuada = 1; $tabuada <= 10; $tabuada++){
+    for($numero = 1; $numero <= 10; $numero++){
+        echo $tabuada." X ".$numero." = ".$tabuada*$numero."<br>";
+    }
+    echo "<hr>";
+}
