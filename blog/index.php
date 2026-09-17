c970eac5ba4fa050ee14b4b1855dbdcf46fa877b
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2026-09-17 17:44:24 -0300

    :sparkles: feat(composer): Configurar o Composer para carregar arquivos automaticamente com autoload
    Oportunidade: Não precisar escrever todos os includes e require no inicio do arquivo para importar
    
    Solução: Configurar o composer com autoload em psr-4 para a pasta sistema onde tem as classes e arquivo de configuração em files
    
    Aprendizado: Instanciar o objeto chamando o caminho\da\Classe, pois o arquivo já foi importado com o composer
    
    Aprendizado²: Além de poder chamar qualquer constante que foi declarada com define no arquivo de configuração

--- blog/index.php
+++ blog/index.php
@@ -1,15 +1,19 @@
 <link rel="stylesheet" href="estilo.css">
 <?php
 
-include 'sistema/configuracao.php';
-include 'sistema/Nucleo/Helpers.php';
-include 'sistema/Nucleo/Mensagem.php';
-include 'sistema/Nucleo/Controlador.php';
-
 Use Sistema\Nucleo\Controlador;
 
-$controlador = new Controlador('sunset');
+include_once 'vendor/autoload.php';
+
+$controlador = new Sistema\Nucleo\Controlador('sunset');
+
+$outraManeiraDeInstanciarObjetoSemCaminho = new Controlador('tipo');
 
 echo '<hr>';
 
 var_dump($controlador);
+var_dump($outraManeiraDeInstanciarObjetoSemCaminho);
+
+echo Sistema\Nucleo\Helpers::saudacao();
+
+echo ANIMAL[1];
---
+++ blog/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php/blog',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0e2a2b00235337d29334d82dd79a1ad5cfdbc9f6',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/blog' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0e2a2b00235337d29334d82dd79a1ad5cfdbc9f6',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
